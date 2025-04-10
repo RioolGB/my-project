2d70fc40134183a6579f8a841e9aef3d026aec0c
Riool <exile-07@inbox.ru>
2025-04-10 16:01:26 +0300

    файл добавлен

---
+++ preobr.py
@@ -0,0 +1,13 @@
+OCT = 8
+BIN = 2
+ZERO = 0
+
+num: int = int(input('Введите число:\n'))
+print(bin(num), oct(num), hex(num))
+for div in BIN, OCT:
+    new_num = num
+    result: str = ''
+    while new_num != ZERO:
+        result = str(new_num % div) + result
+        new_num //= div
+    print(result)
